b2b3ae957e4e9192cb644a3c8546824e04123cee
Joao <joao.levandeira@gmail.com>
2025-01-27 11:43:11 +0000

    adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,23 +1,31 @@
+import userData from "../fixtures/userData.json"
+
+
 describe('Orange HRM Tests', () => {
+  
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField:"[name='password']",
     loginButton: "[type='submit']",
     sectionTittleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-upgrade-layout",
     wrongCredentialAlert: "[role=alert]"
-  }
-  it('Login - Sucecess', () => {
+  }  
+
+ 
+    it('Login - Sucecess', () => {
+
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location(`pathname`).should(`equal`, `/web/index.php/dashboard/index`)
-    cy.get(selectorsList.sectionTittleTopBar).contains(`Dashboard`)
+    cy.get(selectorsList.dashboardGrid)
    })               
   it('Login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('test')
-    cy.get(selectorsList.passwordField).type('test')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
